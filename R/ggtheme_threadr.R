a49b6bac3756b974549b98bf04eed8d89ac8cde0
skgrange <skgrange@gmail.com>
2018-03-10 10:01:25 +0000

    enhance ggplot themer

--- R/ggtheme_threadr.R
+++ R/ggtheme_threadr.R
@@ -1,2 +1,12 @@
 #' @export
-ggtheme_threadr <- function() ggthemes::theme_tufte(base_family = "GillSans")
+ggtheme_threadr <- function(grid_lines = FALSE) {
+  
+  # Tufte styling, maximise data ink
+  plot <- ggthemes::theme_tufte(base_family = "GillSans")
+  
+  # Add grid lines too
+  if (grid_lines) plot <- plot + theme(panel.grid.major = element_line())
+  
+  return(plot)
+  
+}
