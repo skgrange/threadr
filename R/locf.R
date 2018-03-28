b0a1e8f5bef98ef69fc62bece343ca6eaffd46c8
skgrange <skgrange@gmail.com>
2018-03-28 09:38:40 +0100

    clean up na dealing functions & breaking changes due to function name change

--- R/locf.R
+++ R/locf.R
@@ -1,14 +1,13 @@
 #' Function for carrying last observation forwards. 
 #' 
 #' Used to replace each \code{NA} with the most recent non-\code{NA} prior to 
-#' it. \code{locf} is a simple wrapper for \code{zoo::na.locf} with \code{na.rm}
-#' defaulting to \code{FALSE}. 
+#' it. \code{locf} is a simple wrapper for \code{zoo::na.locf}.
 #' 
 #' @param x Input vector. 
 #' 
 #' @param na.rm Should leading NAs be removed? Default is \code{FALSE}. 
 #' 
-#' @return Vector of same data-type and length of \code{x}. 
+#' @return Vector of same data type and length of \code{x}. 
 #' 
 #' @author Stuart K. Grange
 #' 
@@ -21,18 +20,18 @@
 #' }
 #' 
 #' @export
-locf <- function (x, na.rm = FALSE) zoo::na.locf(x, na.rm = na.rm)
+na_locf <- function (x, na.rm = FALSE) zoo::na.locf(x, na.rm = na.rm)
 
 
 #' Function for linearly interpolating \code{NA}s. 
 #' 
 #' @param x Input vector. 
 #' 
-#' @param na.rm Should leading NAs be removed? Default is \code{FALSE}. 
+#' @param na.rm Should leading NAs be removed?
 #' 
 #' @return Vector length of \code{x}. 
 #' 
 #' @author Stuart K. Grange
 #' 
 #' @export
-interpolate_na <- function(x, na.rm = FALSE) zoo::na.approx(x, na.rm = na.rm)
+na_interpolate <- function(x, na.rm = FALSE) zoo::na.approx(x, na.rm = na.rm)
