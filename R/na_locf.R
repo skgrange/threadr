f6dd39f3cc8a4c42fd2001dfc6c01c124c366fd4
skgrange <skgrange@gmail.com>
2018-07-27 08:12:41 +0100

    :zap: enhance na_interpolate

--- R/na_locf.R
+++ R/na_locf.R
@@ -15,7 +15,7 @@
 #' \dontrun{
 #' 
 #' # Push observations forwards, in a data frame
-#' data_income$income_chunk <- locf(data_income$income_chunk)
+#' data_income$income_chunk <- na_locf(data_income$income_chunk)
 #' 
 #' }
 #' 
@@ -23,15 +23,39 @@
 na_locf <- function (x, na.rm = FALSE) zoo::na.locf(x, na.rm = na.rm)
 
 
-#' Function for linearly interpolating \code{NA}s. 
+#' Function for interpolating \code{NA}s. 
+#' 
+#' \code{na_interpolate} wraps functions from the \strong{zoo} package. 
 #' 
 #' @param x Input vector. 
 #' 
 #' @param na.rm Should leading NAs be removed?
 #' 
+#' @param spline Should \code{NA}s be replaced using cubic splines rather than 
+#' linear interpolation. 
+#' 
+#' @param extrapolate Should leading and preceeding \code{NA}s be extrapolated 
+#' too? Only used for linear interpolation.
+#' 
 #' @return Vector length of \code{x}. 
 #' 
 #' @author Stuart K. Grange
 #' 
+#' @seealso \code{\link{na.approx}}
+#' 
 #' @export
-na_interpolate <- function(x, na.rm = FALSE) zoo::na.approx(x, na.rm = na.rm)
+na_interpolate <- function(x, na.rm = FALSE, spline = FALSE, extrapolate = FALSE) {
+  
+  if (!spline) {
+    
+    x <- zoo::na.approx(x, na.rm = na.rm, rule = ifelse(extrapolate, 2, 1))
+    
+  } else {
+    
+    x <- zoo::na.spline(x, na.rm = na.rm)
+    
+  }
+  
+  return(x)
+  
+}
