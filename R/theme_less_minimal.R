8cfe7da23e8241905d5c1494e78dec59b0931362
skgrange <s.k.grange@gmail.com>
2020-02-14 13:08:21 +0100

    :sparkles: make facet margins a bit tighter in theme_less_minimal

--- R/theme_less_minimal.R
+++ R/theme_less_minimal.R
@@ -1,4 +1,4 @@
-#' Function for a mimimal \strong{ggplot2} appearance but with bounding lines 
+#' Function for a minimal \strong{ggplot2} appearance but with bounding lines 
 #' for plot area. 
 #' 
 #' @param base_size Base font size.
@@ -8,6 +8,8 @@
 #' @param base_line_size Base size for line elements. 
 #' 
 #' @param base_rect_size Base size for rectangle elements.
+#' 
+#' @param strip_margin Margins for strip headings. 
 #' 
 #' @author Stuart K. Grange
 #' 
@@ -23,12 +25,14 @@
 #' # Create data
 #' data_example <- tibble(
 #'   x = seq(1:3), 
-#'   y = sample(1:10, 3)
+#'   y = sample(1:10, 3),
+#'   facet_heading = "The plot"
 #' )
 #' 
 #' # Plot with the theme
 #' ggplot(data_example, aes(x, y)) + 
 #'   geom_point() + 
+#'   facet_wrap("facet_heading") + 
 #'   theme_less_minimal()
 #'   
 #' }
@@ -36,7 +40,8 @@
 #' @export
 theme_less_minimal <- function(base_size = 11, base_family = "", 
                                base_line_size = base_size / 22, 
-                               base_rect_size = base_size / 22) {
+                               base_rect_size = base_size / 22,
+                               strip_margin = 2) {
   
   ggplot2::theme_bw(
     base_size = base_size, 
@@ -51,7 +56,13 @@ theme_less_minimal <- function(base_size = 11, base_family = "",
       panel.border = ggplot2::element_rect(fill = NA, linetype = 1, size = 0.1),
       strip.background = ggplot2::element_rect(linetype = 1, size = 0.1),
       plot.background = ggplot2::element_blank(),
-      complete = TRUE
+      complete = TRUE,
+      strip.text.x = ggplot2::element_text(
+        margin = ggplot2::margin(b = strip_margin, t = strip_margin)
+      ),
+      strip.text.y = ggplot2::element_text(
+        margin = ggplot2::margin(b = strip_margin, t = strip_margin)
+      )
     )
   
 }
