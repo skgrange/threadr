6217cb04e6ee04afab3976dcf49fb6d19ac06e42
skgrange <s.k.grange@gmail.com>
2021-03-12 15:44:24 +0100

    :sparkles: add base argument to build_zero_row_tibble

--- R/build_zero_row_tibble.R
+++ R/build_zero_row_tibble.R
@@ -4,16 +4,25 @@
 #' 
 #' @param names Vector of names for the tibble. 
 #' 
+#' @param base Despite the function's name, should a \strong{base} 
+#' \code{data.frame} be returned rather than a tibble? 
+#' 
 #' @return Tibble with names and zero rows. 
 #' 
 #' @export
-build_zero_row_tibble <- function(names) {
+build_zero_row_tibble <- function(names, base = FALSE) {
   
   # Create empty data frame with correct number of variables
-  data.frame(
+  df <- data.frame(
     matrix(0, ncol = length(names), nrow = 0)
   ) %>% 
-    purrr::set_names(names) %>% 
-    as_tibble()
+    purrr::set_names(names) 
+  
+  # To tibble
+  if (!base) {
+    df <- as_tibble(df)
+  }
+  
+  return(df)
   
 }
