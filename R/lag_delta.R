2c24587a0c8d826eab155dbf0d986373217f2493
skgrange <s.k.grange@gmail.com>
2022-08-17 08:54:43 +0200

    :sparkles: add na_as_zero argument to *_delta functions

--- R/lag_delta.R
+++ R/lag_delta.R
@@ -5,6 +5,8 @@
 #' 
 #' @param x Numeric vector. 
 #' 
+#' @param na_as_zero Should missing elements be set to 0? 
+#' 
 #' @return Numeric vector.
 #' 
 #' @examples 
@@ -12,13 +14,17 @@
 #' lag_delta(sample(seq(0:10)))
 #'
 #' @export
-lag_delta <- function(x) {
-  x - dplyr::lag(x)
+lag_delta <- function(x, na_as_zero = FALSE) {
+  x <- x - dplyr::lag(x)
+  if (na_as_zero) x <- ifelse(is.na(x), 0, x)
+  return(x)
 }
 
 
 #' @rdname lag_delta
 #' @export
-lead_delta <- function(x) {
-  x - dplyr::lead(x)
+lead_delta <- function(x, na_as_zero = FALSE) {
+  x <- x - dplyr::lead(x)
+  if (na_as_zero) x <- ifelse(is.na(x), 0, x)
+  return(x)
 }
