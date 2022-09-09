fe73a502985b776d51eeefb745884186447dc7bb
Stuart Grange <s.k.grange@gmail.com>
2022-09-09 13:43:10 +0200

    :sparkles: add file_only_print argument to *_rds functions

--- R/write_rds.R
+++ R/write_rds.R
@@ -8,13 +8,17 @@
 #' 
 #' @param compress Should the RDS file be written with compression? 
 #' 
+#' @param file_only_print If \code{verbose} is \code{TRUE}, should only the file
+#' name be printed without its path? 
+#' 
 #' @param verbose Should the function give messages? Note that the default is 
 #' \code{TRUE}. 
 #' 
 #' @return Invisible x.  
 #' 
 #' @export
-write_rds <- function(x, file, compress = TRUE, verbose = TRUE) {
+write_rds <- function(x, file, compress = TRUE, file_only_print = FALSE, 
+                      verbose = TRUE) {
   
   # Check file
   if (!fs::path_ext(file) %in% c("rds", "RDS")) {
@@ -27,7 +31,16 @@ write_rds <- function(x, file, compress = TRUE, verbose = TRUE) {
   
   # Message after the file have been written
   if (verbose) {
-    message(date_message(), "Exported `", file, "` (`", fs::file_size(file), "`).")
+    
+    # What to print? 
+    if (file_only_print) {
+      file_print <- basename(file)
+    } else {
+      file_print <- file
+    }
+    
+    message(date_message(), "Exported `", file_print, "` (`", fs::file_size(file), "`).")
+    
   }
   
   return(invisible(x))
@@ -37,7 +50,7 @@ write_rds <- function(x, file, compress = TRUE, verbose = TRUE) {
 
 #' @rdname write_rds
 #' @export
-read_rds <- function(file, verbose = TRUE) {
+read_rds <- function(file, file_only_print = FALSE, verbose = TRUE) {
   
   # Check file
   if (!fs::file_exists(file)) {
@@ -50,7 +63,16 @@ read_rds <- function(file, verbose = TRUE) {
   
   # Message the file name and size to user
   if (verbose) {
-    message(date_message(), "Reading `", file, "` (`", fs::file_size(file), "`)...")
+    
+    # What to print? 
+    if (file_only_print) {
+      file_print <- basename(file)
+    } else {
+      file_print <- file
+    }
+    
+    message(date_message(), "Reading `", file_print, "` (`", fs::file_size(file), "`)...")
+    
   }
   
   # Read the file
