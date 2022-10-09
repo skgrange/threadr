fecabad813944c48d2442662ef326717db9307a0
Stuart Grange <s.k.grange@gmail.com>
2022-10-09 10:26:02 +0200

    :sparkles: enhance loess_smoother to predict when there are missing values

--- R/smoother_functions.R
+++ R/smoother_functions.R
@@ -1,27 +1,45 @@
 #' Function to smooth an \emph{x}-\emph{y} pair with loess (local regression). 
 #' 
 #' @param x Numeric vector. 
+#' 
 #' @param y Numeric vector. 
+#' 
 #' @param span Smoother span. 
+#' 
 #' @param degree Degree of the polynomials to be used.
 #' 
-#' @return Numeric vector with the length of \code{x} or \code{y}
+#' @param surface A \code{loess.control} option. Can take the value 
+#' \code{"interpolate"} or \code{"direct"}, If \code{"direct"}, the loess model
+#' will predict outside its training space. 
+#' 
+#' @return Numeric vector with the length of \code{x} or \code{y}. 
 #' 
 #' @author Stuart K. Grange
 #' 
 #' @export
-loess_smoother <- function(x, y, span = 0.75, degree = 2) {
+loess_smoother <- function(x, y, span = 0.75, degree = 2, surface = "interpolate") {
   
   # Date type change if a date
   if (lubridate::is.POSIXt(x)) x <- as.numeric(x)
   
+  # Put vectors into a data frame for modelling and prediction, this allows 
+  # for the prediction to interpolate missing data if they exist
+  df <- data.frame(y = y, x = x)
+  
   # Model, warning suppression is for a low-level n warning
   suppressWarnings(
-    model <- loess(y ~ x, na.action = na.exclude, span = span, degree = degree)
+    model <- loess(
+      y ~ x, 
+      data = df, 
+      span = span, 
+      degree = degree, 
+      control = stats::loess.control(surface = surface),
+      na.action = na.exclude
+    )
   )
   
   # Predict vector
-  smooth <- predict(model, na.action = na.exclude, se = FALSE)
+  smooth <- unname(predict(model, df, se = FALSE))
   
   return(smooth)
   
