81029a40b325223d4b3be5d31849d3672c47c249
Stuart Grange <s.k.grange@gmail.com>
2023-05-14 17:16:59 +0200

    :sparkles: enhance round_any to handle the hms data-type too

--- R/round_any.R
+++ R/round_any.R
@@ -1,8 +1,9 @@
 #' Round to multiple of any number.
 #'
-#' @param x numeric or date-time (POSIXct) vector to round
+#' @param x numeric, date-time (POSIXct), or \strong{hms} vector to round.
 #' 
-#' @param accuracy number to round to; for POSIXct objects, a number of seconds
+#' @param accuracy number to round to; for POSIXct or \strong{hms} objects, a 
+#' number of seconds. 
 #' 
 #' @param f rounding function: \code{\link{floor}}, \code{\link{ceiling}} or
 #'  \code{\link{round}}
@@ -10,6 +11,7 @@
 #' @keywords manip
 #'
 #' @examples
+#' 
 #' round_any(135, 10)
 #' round_any(135, 100)
 #' round_any(135, 25)
@@ -20,9 +22,13 @@
 #' round_any(135, 100, ceiling)
 #' round_any(135, 25, ceiling)
 #'
+#' # For POSIXct data-types
 #' round_any(Sys.time() + 1:10, 5)
 #' round_any(Sys.time() + 1:10, 5, floor)
 #' round_any(Sys.time(), 3600)
+#' 
+#' # For hms data-types
+#' round_any(hms::as_hms(603), 60)
 #' 
 #' @export
 round_any <- function(x, accuracy, f = round) {
@@ -39,4 +45,10 @@ round_any.POSIXct <- function(x, accuracy, f = round) {
   tz <- format(x[1], "%Z")
   xr <- round_any(as.numeric(x), accuracy, f)
   as.POSIXct(xr, origin="1970-01-01 00:00.00 UTC", tz = tz)
+}
+
+#' @export
+round_any.hms <- function(x, accuracy, f = round) {
+  xr <- round_any(as.numeric(x), accuracy, f)
+  hms::as_hms(xr)
 }
