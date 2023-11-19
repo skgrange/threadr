931099546d970c355dfb4320647366f947ea7139
Stuart Grange <s.k.grange@gmail.com>
2023-11-19 20:50:08 +0100

    :book: enhance documentation for mode_average

--- R/mode_average.R
+++ R/mode_average.R
@@ -1,20 +1,38 @@
-#' Function to calculate mode of a vector. 
+#' Function to calculate mode (the value that appears most often) of a vector.
 #' 
-#' @param x Input vector. 
-#' @param na.rm Should \code{NA}s be omitted? Deafult is \code{FALSE}. 
+#' @author Ken Williams. 
 #' 
-#' @return Vector with a length of one. 
+#' @param x Input vector.
+#' 
+#' @param na.rm Should \code{NA}s be omitted?
+#' 
+#' @return Vector with a length of \code{1}. 
 #' 
 #' @seealso \href{http://stackoverflow.com/questions/2547402/is-there-a-built-in-function-for-finding-the-mode}{stackoverflow},
 #' \code{\link{mean}}, \code{\link{median}}
 #' 
-#' @author Ken Williams with enhancement by Stuart K. Grange
+#' @examples
+#' 
+#' # Calculate the mode of integers
+#' mode_average(c(1L, 2L, 3L, 3L))
+#' 
+#' # Calculate the mode of a numeric vector
+#' mode_average(c(5, 6, 6, 500000))
+#' 
+#' # Will return the first element when one or two elements are supplied
+#' mode_average(c(5, 1))
+#' mode_average(c("hail", "rain"))
+#' 
+#' # Will also work with character and factor vectors
+#' mode_average(c("rain", "hail", "sunshine", "rain"))
 #' 
 #' @export
 mode_average <- function(x, na.rm = FALSE) {
   
+  # Drop missing elements
   if (na.rm) x <- na.omit(x)
   
+  # Compute the mode of the vector
   ux <- unique(x)
   ux[which.max(tabulate(match(x, ux)))]
   
