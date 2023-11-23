79ca7a90c3f6472f52d7bb91c9d0e4774773d984
skgrange <skgrange@gmail.com>
2023-11-23 10:01:20 +0100

    :sparkles: add split_into_equal_parts

--- R/percentage_change.R
+++ R/percentage_change.R
@@ -6,9 +6,11 @@
 #' 
 #' @param as_decimal Should the percentage be represented in a 0 to 1 scale? 
 #' 
-#' @return Numeric vector.
+#' @return Numeric vector. 
 #' 
 #' @author Stuart K. Grange
+#' 
+#' @seealso \code{\link{percentage_difference}}
 #' 
 #' @examples 
 #' 
@@ -16,15 +18,49 @@
 #' percentage_change(100, 50)
 #' percentage_change(50, 100)
 #' 
-#' # Global mean CO2 concentrations
+#' # Global mean CO2 mole fraction
 #' percentage_change(277, 400)
 #' percentage_change(277, 400, as_decimal = TRUE)
+#' 
+#' # Percentage change depends on the order of the inputs because it depends on
+#' # the denominator
+#' percentage_change(104, 130)
+#' percentage_change(130, 104)
 #' 
 #' @export
 percentage_change <- function(previous, new, as_decimal = FALSE) {
   x <- ((new - previous) / previous) * 100
   if (as_decimal) x <- x / 100
   return(x)
+}
+
+
+#' Function to calculate the percentage difference between two values. 
+#' 
+#' @param value Numeric vector of values.  
+#' 
+#' @param value_two Numeric vector of values.  
+#' 
+#' @return Numeric vector with length of \code{value}.
+#' 
+#' @author Stuart K. Grange
+#' 
+#' @seealso \code{\link{percentage_change}}
+#' 
+#' @examples 
+#' 
+#' # Calculate some differences
+#' percentage_difference(100, 80)
+#' percentage_difference(104, 130)
+#' 
+#' # A percentage difference's sign changes when the order of the inputs change,
+#' # but not the value
+#' percentage_difference(104, 130)
+#' percentage_difference(130, 104)
+#' 
+#' @export
+percentage_difference <- function(value, value_two) {
+  (value - value_two) / ((value + value_two) / 2) * 100
 }
 
 
@@ -75,25 +111,3 @@ percent_lost <- function(value, value_two, as_decimal = FALSE) {
   if (as_decimal) x <- x / 100
   return(x)
 }
-
-
-#' Function to calculate the percentage difference between two values. 
-#' 
-#' @param value Numeric vector of values.  
-#' 
-#' @param value_two Numeric vector of values.  
-#' 
-#' @return Numeric vector with length of \code{value}.
-#' 
-#' @author Stuart K. Grange
-#' 
-#' @examples 
-#' 
-#' # Calculate some differences
-#' percentage_difference(100, 80)
-#' percentage_difference(104, 130)
-#' 
-#' @export
-percentage_difference <- function(value, value_two) {
-  (value - value_two) / ((value + value_two) / 2) * 100
-} 
--- R/split_nrow.R
+++ R/split_nrow.R
@@ -22,18 +22,46 @@
 split_nrow <- function(df, rows) {
   
   if (any(grepl("data.frame", class(df)))) {
-    
     # Split data frame into a list with n elements
     suppressWarnings(
       list_split <- split(df, (0:nrow(df) %/% rows))
     )
-    
   } else if (any(class(df) %in% c("character", "integer", "numeric", "factor"))) {
-    
     list_split <- split(df, ceiling(seq_along(df) / rows))
-    
   }
   
   return(list_split)
+  
+}
+
+
+#' Function to spit a data frame/tibble into equal sized parts/pieces. 
+#' 
+#' The final part may have a different number of rows than all other parts 
+#' because of the length of the input cannot always be divided equality. 
+#' 
+#' @author Stuart K. Grange
+#' 
+#' @param df Data frame or tibble to be split. 
+#' 
+#' @param n_parts Number of parts/pieces to split \code{df} into. 
+#' 
+#' @return A named list with the length of \code{n_parts}. 
+#' 
+#' @export
+split_into_equal_parts <- function(df, n_parts = 10) {
+  
+  stopifnot(inherits(df, "data.frame"))
+  
+  # Get number of rows of the input
+  nrows <- nrow(df)
+  
+  # Create a grouping integer
+  x <- rep(seq_len(n_parts), length.out = nrows, each = ceiling(nrows / n_parts))
+  
+  # Do the splitting
+  list_df <- split(df, x)
+  
+  return(list_df)
   
 }
