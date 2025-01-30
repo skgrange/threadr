0a51dc033e2c6c27e78663dfe5b28d30e59b6a7f
skgrange <s.k.grange@gmail.com>
2025-01-31 08:03:15 +1000

    :sparkles: enhance transform_dates_with_time_zone with some new arguments & replace variables by default

--- R/transform_dates_with_time_zone.R
+++ R/transform_dates_with_time_zone.R
@@ -10,12 +10,17 @@
 #' 
 #' @param df Tibble with \code{date} and \code{time_zone} variables. 
 #' 
-#' @param warn Should the function raise warnings? 
+#' @param dates_replace Should the function replace/overwrite the original 
+#' \code{date} variable(s) with the transformed version? 
+#' 
+#' @param drop_time_zone Should the helper \code{time_zone} variable be dropped
+#' in the return? 
 #' 
 #' @return \code{df} with transformed date variables. 
 #' 
 #' @export
-transform_dates_with_time_zone <- function(df, warn = TRUE) {
+transform_dates_with_time_zone <- function(df, dates_replace = TRUE,
+                                           drop_time_zone = FALSE) {
   
   # Check inputs
   stopifnot(c("date", "time_zone") %in% names(df), lubridate::is.POSIXt(df$date))
@@ -23,14 +28,23 @@ transform_dates_with_time_zone <- function(df, warn = TRUE) {
   # Do the transformations, do each time zone separately
   df <- df %>% 
     dplyr::group_split(time_zone) %>% 
-    purrr::map(transform_dates_with_time_zone_worker, warn = warn) %>% 
-    purrr::list_rbind() %>% 
-    relocate(date_in_time_zone,
-             .after = date)
+    purrr::map(
+      transform_dates_with_time_zone_worker, dates_replace = dates_replace
+    ) %>% 
+    purrr::list_rbind()
   
-  # Also relocate date_end if it exits
-  if ("date_end" %in% names(df)) {
+  # Relocate transformed date variables
+  if ("date_in_time_zone" %in% names(df)) {
+    df <- relocate(df, date_in_time_zone, .after = date)
+  }
+  
+  if ("date_end_in_time_zone" %in% names(df)) {
     df <- relocate(df, date_end_in_time_zone, .after = date_end)
+  }
+  
+  # Drop helper variable too
+  if (drop_time_zone) {
+    df <- select(df, -time_zone)
   }
   
   return(df)
@@ -38,7 +52,7 @@ transform_dates_with_time_zone <- function(df, warn = TRUE) {
 }
 
 
-transform_dates_with_time_zone_worker <- function(df, warn) {
+transform_dates_with_time_zone_worker <- function(df, dates_replace) {
   
   # Get time zone
   time_zone <- unique(df$time_zone)
@@ -46,30 +60,44 @@ transform_dates_with_time_zone_worker <- function(df, warn) {
   # Check for uniqueness
   stopifnot(length(time_zone) == 1L)
   
-  # Convert dates
-  df <- df %>% 
-    mutate(date_in_time_zone = lubridate::with_tz(date, tz = time_zone),
-           date_in_time_zone = lubridate::force_tz(date_in_time_zone, tz = "UTC"))
+  # Does the input have a date end too?
+  has_date_end <- "date_end" %in% names(df)
+  
+  # Convert date vectors
+  date_transformed <- df %>% 
+    pull(date) %>% 
+    with_and_force_tz(time_zone)
   
   # Also convert date_end if the variable exists
-  if ("date_end" %in% names(df)) {
-    df <- df %>% 
-      mutate(
-        date_end_in_time_zone = lubridate::with_tz(date_end, tz = time_zone),
-        date_end_in_time_zone = lubridate::force_tz(date_end_in_time_zone, tz = "UTC")
-      )
+  if (has_date_end) {
+    date_end_transformed <- df %>% 
+      pull(date) %>% 
+      with_and_force_tz(time_zone)
   }
   
-  # Check for duplicated dates after conversion
-  if (warn && any_duplicated(df$date_in_time_zone)) {
-    cli::cli_alert_warning("Duplicated dates detected...")
-  }
-  
-  if (warn && "date_end" %in% names(df) && 
-      any_duplicated(df$date_end_in_time_zone)) {
-    cli::cli_alert_warning("Duplicated `date_end` values detected...")
+  # Add date vectors to data frame, replace or add
+  if (dates_replace) {
+    df <- mutate(df, date = !!date_transformed)
+    if (has_date_end) {
+      df <- mutate(df, date_end = !!date_end_transformed)
+    }
+  } else {
+    df <- mutate(df, date_in_time_zone = !!date_transformed)
+    if (has_date_end) {
+      df <- mutate(df, date_end_in_time_zone = !!date_end_transformed)
+    }
   }
   
   return(df)
+  
+}
+
+
+with_and_force_tz <- function(date, time_zone, time_zone_target = "UTC") {
+  
+  # Convert dates and force time zone after conversion
+  date %>% 
+    lubridate::with_tz(tzone = time_zone) %>% 
+    lubridate::force_tz(tzone = time_zone_target)
   
 }
