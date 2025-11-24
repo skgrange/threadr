4d44f8d92ec4aab11e779896c90121cfba9c5ec2
skgrange <s.k.grange@gmail.com>
2025-11-24 15:47:13 +0100

    :sparkles: add linear_fit & clean up smoother functions

--- R/smoother_functions.R
+++ R/smoother_functions.R
@@ -1,5 +1,7 @@
 #' Function to smooth an \emph{x}-\emph{y} pair with loess (local regression). 
 #' 
+#' @author Stuart K. Grange
+#' 
 #' @param x Numeric vector. 
 #' 
 #' @param y Numeric vector. 
@@ -14,17 +16,18 @@
 #' 
 #' @return Numeric vector with the length of \code{x} or \code{y}. 
 #' 
-#' @author Stuart K. Grange
-#' 
 #' @export
-loess_smoother <- function(x, y, span = 0.75, degree = 2, surface = "interpolate") {
+loess_smoother <- function(x, y, span = 0.75, degree = 2, 
+                           surface = "interpolate") {
   
   # Date type change if a date
-  if (lubridate::is.POSIXt(x)) x <- as.numeric(x)
+  if (lubridate::is.POSIXt(x)) {
+    x <- as.numeric(x)
+  }
   
   # Put vectors into a data frame for modelling and prediction, this allows 
   # for the prediction to interpolate missing data if they exist
-  df <- data.frame(y = y, x = x)
+  df <- tibble(y = y, x = x)
   
   # Model, warning suppression is for a low-level n warning
   suppressWarnings(
@@ -39,9 +42,9 @@ loess_smoother <- function(x, y, span = 0.75, degree = 2, surface = "interpolate
   )
   
   # Predict vector
-  smooth <- unname(predict(model, df, se = FALSE))
+  value_predict <- unname(predict(model, df, se = FALSE))
   
-  return(smooth)
+  return(value_predict)
   
 }
 
@@ -49,31 +52,70 @@ loess_smoother <- function(x, y, span = 0.75, degree = 2, surface = "interpolate
 #' Function to smooth an \emph{x}-\emph{y} pair with a generalised additive 
 #' model (GAM).
 #' 
+#' @author Stuart K. Grange
+#' 
 #' @param x Numeric vector. 
+#' 
 #' @param y Numeric vector. 
+#' 
 #' @param method Smoothing estimation method. 
 #' 
-#' @return Numeric vector with the length of \code{x} or \code{y}
-#' 
-#' @author Stuart K. Grange
+#' @return Numeric vector with the length of \code{x} or \code{y}.
 #' 
 #' @export
 gam_smoother <- function(x, y, method = "GCV.Cp") {
   
   # Date type change if a date
-  if (lubridate::is.POSIXt(x)) x <- as.numeric(x)
+  if (lubridate::is.POSIXt(x)) {
+    x <- as.numeric(x)
+  }
   
   # Model
   model <- mgcv::gam(
     y ~ s(x, bs = "cr"), na.action = na.exclude, method = method
   )
   
-  # Predict vector
-  smooth <- predict(model)
+  # Predict
+  value_predict <- predict(model)
   
   # Drop names
-  attributes(smooth) <- NULL
+  attributes(value_predict) <- NULL
   
-  return(smooth)
+  return(value_predict)
+  
+}
+
+
+#' Function to predict the linear function of \emph{x}-\emph{y} pair. 
+#' 
+#' @author Stuart K. Grange
+#' 
+#' @param x Numeric vector. 
+#' 
+#' @param y Numeric vector. 
+#' 
+#' @param robust Should robust linear regression be used rather than ordinary 
+#' least squares? 
+#' 
+#' @return Numeric vector with the length of \code{x} or \code{y}. 
+#' 
+#' @export
+linear_fit <- function(x, y, robust = FALSE) {
+  
+  # Put vectors into a data frame for modelling and prediction
+  df <- tibble(y = y, x = x)
+  
+  # Train model
+  if (robust) {
+    model <- MASS::rlm(y ~ x, data = df)
+  } else {
+    model <- lm(y ~ x, data = df)
+  }
+  
+  # Predict vector
+  value_predict <- unname(predict(model, df, se = FALSE))
+  
+  return(value_predict)
+  
   
 }
